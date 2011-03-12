691887ff498037db23fb05987dac81c50c1982ff
Andras Ivanyi <andras.ivanyi@gmail.com>
2011-03-12 16:30:34 +0100

    Initial alarm sound added to the project.

--- src/hu/harmakhis/shisha/main.java
+++ src/hu/harmakhis/shisha/main.java
@@ -1,11 +1,13 @@
 package hu.harmakhis.shisha;
 
+import java.io.IOException;
+
 import hu.harmakhis.shisha.entities.Session;
 import hu.harmakhis.shisha.utils.IntentManager;
-import hu.harmakhis.shisha.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
@@ -22,6 +24,7 @@ public class main extends Activity {
 	private Chronometer cm;
 	private TextView actPlayer;
 	LinearLayout ll;
+	private MediaPlayer mp;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +33,7 @@ public class main extends Activity {
         s.setWarnTimeOut(60*1000*5);
         s.addPlayer("andy");
         s.addPlayer("flyerz");
-
+        mp = MediaPlayer.create(this, R.raw.timeout);
         cm = (Chronometer) findViewById(R.id.chronometer1);
         actPlayer= (TextView) findViewById(R.id.textView1);
         Button b = (Button) findViewById(R.id.button1);
@@ -56,6 +59,7 @@ public class main extends Activity {
 				long time = SystemClock.elapsedRealtime()-cm.getBase();
 				if (time > s.getWarnTimeOut()) {
 					cm.setTextColor(Color.RED);
+					if (!mp.isPlaying()) mp.start();
 				}		
 			}
 		});
